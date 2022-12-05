be620b2933f5ededa18e19dfbc8530485ee264a0
vic <phalphie1@gmail.com>
2022-12-05 10:17:12 +0100

    document's editor

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Reader from "./components/DocReacder/Reader";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Reader/>
+    </div>
+  );
+}
+
---
+++ src/components/DocReacder/utils/PDFLib.js
@@ -0,0 +1,3 @@
+import "//mozilla.github.io/pdf.js/build/pdf.js";
+const PDFLib = window['pdfjs-dist/build/pdf'];
+export default PDFLib;
